3901be378921f7823cdad3a4c268c90a97fc8285
Duye Chen <k078264@gmail.com>
2015-07-02 18:39:35 +0800

    README and upload Arduino file

---
+++ Arduino/GTArm.ino
@@ -0,0 +1,82 @@
+#include <Servo.h>
+
+int value = 0 ;
+int vec_angle[4];
+char s;
+Servo servo_pin_11;
+Servo servo_pin_10;
+Servo servo_pin_9;
+Servo servo_pin_5;
+
+void turnservo();
+void initarray();
+void initservo();
+
+void setup()
+{
+   Serial.begin(9600);
+  for (int i=0;i<4;i++) vec_angle[i]=0;
+
+  servo_pin_11.attach(11);
+  servo_pin_10.attach(10);
+  servo_pin_9.attach(9);
+  servo_pin_5.attach(5);
+  initarray();
+  initservo();
+}
+
+void loop()
+{
+  if (Serial.available() > 0) 
+  {
+    s = Serial.read(); 
+  }
+   switch(s)
+    {
+      case('R'):vec_angle[0]=vec_angle[0]-1;break;
+      case('L'):vec_angle[0]=vec_angle[0]+1;break;
+      case('U'):vec_angle[2]=vec_angle[2]+1;break;
+      case('D'):vec_angle[2]=vec_angle[2]-1;break;
+      case('S'):vec_angle[1]=vec_angle[1]+1;break;
+      case('B'):vec_angle[1]=vec_angle[1]-1;break;
+      case('C'):vec_angle[3]=vec_angle[3]-1;break;
+      case('Y'):vec_angle[3]=vec_angle[3]+1;break;
+      default:;break;
+    }
+    if(vec_angle[0]<20)vec_angle[0]=20;
+    else if(vec_angle[0]>160)vec_angle[0]=160;
+    if(vec_angle[3]<112)vec_angle[3]=112;
+    else if(vec_angle[3]>160)vec_angle[3]=160;
+    if(vec_angle[2]<75)vec_angle[2]=75;
+    else if(vec_angle[2]>175)vec_angle[2]=175;
+    if(vec_angle[1]<80)vec_angle[1]=80;
+    else if(vec_angle[1]>145)vec_angle[1]=145;
+  turnservo();
+  delay( 30 );
+}
+
+void initarray()
+{
+  vec_angle[0] = 90 ;
+  vec_angle[1] = 90 ;
+  vec_angle[2] = 90 ;
+  vec_angle[3] = 112 ;
+}
+
+void turnservo()
+{
+  servo_pin_11.write( vec_angle[0] );
+  servo_pin_10.write( vec_angle[1] );
+  servo_pin_9.write( vec_angle[2] );
+  servo_pin_5.write( vec_angle[3] );
+}
+
+void initservo()
+{
+  servo_pin_11.write( vec_angle[0] );
+  servo_pin_10.write( vec_angle[1] );
+  servo_pin_9.write( vec_angle[2] );
+  servo_pin_5.write( vec_angle[3] );
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # GTArm
+
+6月多時參加南部 Maker 展時所製作的機械手臂控制器，原本只有單純的控制，最後莫名其妙的變成了用臉去控制手臂移動的方向，然後在控制端上顯示手臂上攝影機所拍得的畫面。
+
+## 使用
+
+要啟動控制器，首先你得要擁有手臂的 Arduino 控制程式，這部分你可以在 Arduino 資料夾中找到。
+
+之後你還需要一台裝有 NodeJS 的電腦，並且安裝以下套件：
+
+	npm install express socket.io mime serialport peterbraden/node-opencv
+
+然後修改這行
+
+	var serialPort = new SerialPort("/dev/tty.usbserial"
+
+將`/dev/tty.usbserial`改成你電腦與手臂所連結的Port。
+
+之後，在終端中輸入
+
+	node server.js
+
+就會執行本程式了。
+
+執行前記得裝上至少一個 USB 攝影機，在 server.js 程式碼中可以關閉手臂的第二個攝影機，只要把這行註解掉即可。
+
+	var armCamera = new cv.VideoCapture(1);
+
+## 硬體
+
+- [MeArm.Joystick](http://mearm.weebly.com)
+- [Arduino Uno](https://www.arduino.cc)
+- MacBook Air
+- USB Camera
+
+## 開發者
+
+手臂硬體程式：[Single Zero](http://single9.net)
+
+電腦控制端：[Duye Chen](http://single9.net)
